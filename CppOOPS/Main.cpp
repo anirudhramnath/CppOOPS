d28aab80a7e99397b6f02a2745c3154b98b50232
Anirudh Ramnath <anirudh@10-4-239-33.vlan890.client.wireless.uic.edu>
2016-03-01 23:02:33 -0600

    finished

--- CppOOPS/Main.cpp
+++ CppOOPS/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <regex>
-#include <typeinfo>
+#include <fstream>
 #include "AbstractCollection.h"
 #include "OrderedList.h"
 #include "UnorderedList.h"
@@ -26,8 +26,211 @@ Any other input line will generate an error message\n\n"
 
 using namespace std;
 
-int main() {
+AbstractCollection* lists[8] = {NULL}; // Stores all lists
 
+void process_input( std::istream  * stream){
+    // Strings used for regex
+    regex INIT_LIST ("[ ]*s[ ]+([1-8])[ ]+(u|o|q|s)[ ]*");
+    regex INSERT_LIST ("[ ]*i[ ]+([1-8])[ ]+(\\d+)[ ]*");
+    regex REMOVE_VAL ("[ ]*r[ ]+([1-8])[ ]*");
+    regex LIST_INFO ("[ ]*l[ ]+([1-8])[ ]*");
+    regex GET_VAL ("[ ]*g[ ]+([1-8])[ ]*");
+    regex COMMENT ("[ ]*#.*");
+    regex FILE_NAME ("[ ]*f[ ]+(\.+)[ ]*");
+    cout<<"process_input"<<endl;
+    Stack * temp_stack;
+    Queue * temp_queue;
+    OrderedList * temp_ol;
+    UnorderedList * temp_ul;
+
+    cout<< "Enter operation or enter 'h' to view various commands being used for this program\n\n";
+    string operation;
+
+    //getline(*stream, operation);
+
+    do {
+        getline(*stream, operation);
+        cout<<operation<<endl;
+        smatch match;  // Regex matcher object
+        if(operation.compare("q") == 0){
+            cout<<"exiting for quit:"<<operation<<endl;
+            exit(0);
+        }else if (operation.compare("h") == 0) {
+            cout << HELP;
+        }
+        else if (operation.compare("a") == 0) {
+            cout << ABOUT;
+        }
+        // Match COMMENT
+        else if (regex_match(operation , match , COMMENT)) {
+            cout << "Ignoring comment"<< endl;
+        }
+        // Match INIT_LIST
+        else if (regex_match(operation , match , INIT_LIST)) {
+            string datatype = match[2];
+            int listNum = stoi(match[1]) - 1;
+            if (datatype.compare("s") == 0)
+                lists[listNum] = new Stack();
+            else if (datatype.compare("q") == 0)
+                lists[listNum] = new Queue();
+            else if (datatype.compare("o") == 0)
+                lists[listNum] = new OrderedList();
+            else if (datatype.compare("u") == 0)
+                lists[listNum] = new UnorderedList();
+        }
+        // Match INSERT_LIST
+        else if (regex_match(operation , match , INSERT_LIST)) {
+            int valueToInsert = stoi(match[2]);
+            int listNum = stoi(match[1]) - 1;
+
+            if (NULL == lists[listNum]) {
+                cout << "List number "<<listNum<<" is empty!"<<endl;
+            }
+            else {
+                lists[listNum]->insert(valueToInsert);
+            }
+        }
+        // Match REMOVE_VAL
+        else if (regex_match(operation , match , REMOVE_VAL)) {
+            int list_index = stoi(match[1]) - 1;
+            int remove_index;
+            if (NULL == lists[list_index]) {
+                cout << "List number "<<list_index+1<<" is empty!"<<endl;
+            }else {
+                string remove_index_str;
+                try{
+                    Stack * temp_stack;
+                    Queue * temp_queue;
+                    OrderedList * temp_ol;
+                    UnorderedList * temp_ul;
+                if(temp_queue = dynamic_cast<Queue*>(lists[list_index])){
+                    lists[list_index]->remove();
+                }
+                else if(temp_stack = dynamic_cast<Stack*>(lists[list_index])){
+                    lists[list_index]->remove();
+                }
+                else if(temp_ul = dynamic_cast<UnorderedList*>(lists[list_index])){
+                    try{
+                        cout<<"Enter the index of the element to be removed:";
+                        getline(*stream, remove_index_str);
+                        remove_index = stoi(remove_index_str);
+                        stream->clear();
+                        lists[list_index]->remove(remove_index -1);
+                    }
+                    catch(invalid_argument){
+                        cout<<"Invalid arguments"<<endl;
+                    }
+                }
+                else if(temp_ol = dynamic_cast<OrderedList*>(lists[list_index])){
+                    try{
+                        cout<<"Enter the index of the element to be removed:";
+                        getline(*stream, remove_index_str);
+                        remove_index = stoi(remove_index_str);
+                        stream->clear();
+                        lists[list_index]->remove(remove_index -1);
+                    }
+                    catch(invalid_argument){
+                            cout<<"Invalid arguments"<<endl;
+                    }
+                }
+                }
+                catch(IndexOutOfBound)
+                {
+                    cout<<"Index out of range";
+                }
+                cout << "Matched REMOVE_VAL"<<endl;
+            }
+        }
+        // Match LIST_INFO
+        else if (regex_match(operation , match , LIST_INFO)) {
+            int listNum = stoi(match[1]) - 1;
+
+            if (NULL == lists[listNum]) {
+                cout << "List number "<<listNum+1<<" is empty!"<<endl;
+            }
+            else {
+                cout << lists[listNum]->toString()<<endl;
+            }
+        }
+        // Match GET_VAL
+        else if (regex_match(operation , match , GET_VAL)) {
+            cout << "Matched GET_VAL"<<endl;
+            int list_index = stoi(match[1]) - 1;
+            int remove_index;
+
+            if (NULL == lists[list_index]) {
+                cout << "List number "<<list_index+1<<" is empty!"<<endl;
+            }
+            else {
+                string remove_index_str;
+                try{
+                    Stack * temp_stack;
+                    Queue * temp_queue;
+                    OrderedList * temp_ol;
+                    UnorderedList * temp_ul;
+                if(temp_queue = dynamic_cast<Queue*>(lists[list_index])){
+                    cout<<lists[list_index]->get()<<endl;
+                }
+                else if(temp_stack = dynamic_cast<Stack*>(lists[list_index])){
+                    cout<<lists[list_index]->get()<<endl;
+                }
+                else if(temp_ul = dynamic_cast<UnorderedList*>(lists[list_index])){
+                    try{
+                        cout<<"Enter the index of the element to be returned:";
+                        getline(*stream, remove_index_str);
+                        remove_index = stoi(remove_index_str);
+                        stream->clear();
+                        cout<<(lists[list_index]->get(remove_index - 1))<<endl;
+                    }
+                    catch(invalid_argument){
+                        cout<<"Invalid arguments"<<endl;
+                    }
+                }
+                else if(temp_ol = dynamic_cast<OrderedList*>(lists[list_index])){
+                    try{
+                        cout<<"Enter the index of the element to be returned:";
+                        getline(*stream, remove_index_str);
+                        remove_index = stoi(remove_index_str);
+                        stream->clear();
+                        cout<< "Remove index is "<<remove_index-1 << endl;
+                        cout<<(lists[list_index]->get(remove_index - 1))<<endl;
+                    }
+                    catch(invalid_argument){
+                        cout<<"Invalid arguments"<<endl;
+                    }
+                }
+                }
+                catch(IndexOutOfBound)
+                {
+                    cout<<"Index out of range";
+                }
+            }
+        }
+        else if (regex_match(operation , match , FILE_NAME)) {
+            ifstream fs(match[1]);
+            cout<<"Invalid File"<<endl;
+            
+            process_input(&fs);
+            
+        }
+        else {
+            cout << "Invalid input"<<endl;
+            cout << "input was : "<<operation<<endl;
+        }
+
+        cout << "Enter operation or enter 'h' to view various commands being used for this program\n\n";
+        
+        
+    }while(!stream->eof());
+
+    cout << "Quitting program"<<endl;
+
+}
+
+int main() {
+    while (1 == 1)
+        process_input(&cin);
+    /*
     // Strings used for regex
     regex INIT_LIST ("[ ]*s[ ]+([1-8])[ ]+(u|o|q|s)[ ]*");
     regex INSERT_LIST ("[ ]*i[ ]+([1-8])[ ]+(\\d+)[ ]*");
@@ -193,6 +396,7 @@ int main() {
     }
 
     cout << "Quitting program"<<endl;
-
+*/
     return 1;
 }
+
